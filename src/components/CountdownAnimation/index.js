dddf23df1986be89e2538f5be990b7fe5a8e4106
Muriel Damazio <murieldps@gmail.com>
2021-10-13 13:55:21 -0300

    Pomodoro App

---
+++ src/components/CountdownAnimation/index.js
@@ -0,0 +1,29 @@
+import { useContext } from 'react';
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
+import { SettingsContext } from '../../context/SettingsContext';
+const CountdownAnimation = ({ key, timer, animate, children }) => {
+  const { stopAimate } = useContext(SettingsContext);
+
+  return (
+    <CountdownCircleTimer
+      key={key}
+      isPlaying={animate}
+      duration={timer * 60}
+      colors={[
+        ['#00ffbf', 0.33],
+        ['#00ffbf', 0.33],
+        ['#00ffbf', 0.33],
+      ]}
+      strokeWidth={8}
+      size={335}
+      trailColor='#ccc'
+      onComplete={() => {
+        stopAimate();
+      }}
+    >
+      {children}
+    </CountdownCircleTimer>
+  );
+};
+
+export default CountdownAnimation;
